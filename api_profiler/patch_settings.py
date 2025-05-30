e96e033127360701dd4430789405a02264202ab2
Abhishek <abhishekghorashainee07@gmail.com>
2025-05-30 17:34:07 +0000

    fix: refactor patch_settings.py for clarity and adjust WSGI_APPLICATION setup; bump version to 0.1.6 in setup.py

--- api_profiler/patch_settings.py
+++ api_profiler/patch_settings.py
@@ -1,22 +1,29 @@
 from importlib import import_module
 import os
 
-from api_profiler.django_utils.discover_app import DiscoverApp 
 
 original_settings = os.environ.get("ORIGINAL_DJANGO_SETTINGS_MODULE")
 
 
-project_name = DiscoverApp.get_app_name()
-if not project_name:
-    raise RuntimeError("Run this script from the project root directory.")
+print(original_settings)
 
+"""
+while importing original module setting django settings module to original module so that 
+django settings is loaded properly as expected
+"""
+os.environ["DJANGO_SETTINGS_MODULE"]=original_settings
 settings = import_module(original_settings)
 
 globals().update({k: getattr(settings, k) for k in dir(settings) if k.isupper()})
 
-WSGI_APPLICATION = f"{project_name}.wsgi.application"
 
 #Inject middleware 
 middleware_path = "api_profiler.middleware.Profiler"
 if middleware_path not in MIDDLEWARE: # type: ignore
     MIDDLEWARE.append(middleware_path) # type: ignore
+
+from api_profiler.django_utils.discover_app import DiscoverApp 
+project_name = DiscoverApp.get_app_name()
+if not project_name:
+    raise RuntimeError("Run this script from the project root directory.")
+WSGI_APPLICATION = f"{project_name}.wsgi.application"
